f555c773203fdb84e10af8f5c01539aa79326d02
melis1986 <aldaiarovmelis@gmail.com>
2025-06-03 20:54:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from check_google_sheet import get_sheet_data
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "WhatsApp CRM Bot is running!"}
+    try:
+        data = get_sheet_data()
+        return {"message": "CRM данные успешно получены", "data": data}
+    except Exception as e:
+        return {"error": str(e)}
