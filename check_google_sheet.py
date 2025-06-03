628cf3087e5cbf88310f6ae123b8743cd73f4e5e
melis1986 <aldaiarovmelis@gmail.com>
2025-06-03 21:59:18 +0400

    Update check_google_sheet.py

--- check_google_sheet.py
+++ check_google_sheet.py
@@ -1,19 +1,24 @@
-def get_sheet_data():
-    import gspread
-    from google.oauth2.service_account import Credentials
-    import json
+import gspread
+from google.oauth2.service_account import Credentials
 
-    creds_path = "/etc/secrets/service_account.json"
-   scopes = [
-    "https://www.googleapis.com/auth/spreadsheets.readonly",
-    "https://www.googleapis.com/auth/drive.readonly"
+# Путь к JSON-файлу
+SERVICE_ACCOUNT_FILE = '/etc/secrets/service_account.json'
+
+# Указываем права доступа
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
 ]
-    with open(creds_path) as source:
-        creds_dict = json.load(source)
 
-    credentials = Credentials.from_service_account_info(creds_dict, scopes=scopes)
-    gc = gspread.authorize(credentials)
+credentials = Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE,
+    scopes=scopes
+)
 
-    spreadsheet = gc.open_by_key("1YHAhKeKzT5in87uf1d5vCt0AnXllhXl4PemviXbPxNE")
-    worksheet = spreadsheet.sheet1
-    return worksheet.get_all_records()
+# Новый ID таблицы
+spreadsheet_id = "1YHAhKeKzT5in87uf1d5VcT0AnXllhXl4PemviXbPxNE"
+
+# Подключаемся и читаем первую строку
+client = gspread.authorize(credentials)
+sheet = client.open_by_key(spreadsheet_id).worksheet("Лист1")
+print(sheet.row_values(1))
