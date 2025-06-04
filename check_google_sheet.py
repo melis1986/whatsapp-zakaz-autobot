59ab7b258a2b5f0309db89cd326caea0e9de5c1d
melis1986 <aldaiarovmelis@gmail.com>
2025-06-04 10:30:45 +0400

    Update check_google_sheet.py

--- check_google_sheet.py
+++ check_google_sheet.py
@@ -1,24 +1,29 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Путь к JSON-файлу (если он на сервере Render)
+# Путь к JSON-файлу на Render
 SERVICE_ACCOUNT_FILE = '/etc/secrets/service_account.json'
 
-# Указываем права доступа
-scopes = [
+# Правильные OAuth scopes
+SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
+
+# Создаем объект credentials
 credentials = Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE,
-    scopes=scopes
+    scopes=SCOPES
 )
 
-# Новый ID таблицы
-spreadsheet_id = '1YHAhKeKzT5in87uf1d5VcT0AnXllhXL4PemviXbPxNE'
-
-# Подключаемся и читаем первую строку
+# Авторизация клиента
 client = gspread.authorize(credentials)
+
+# ID таблицы (оставь свой)
+spreadsheet_id = "1YHAhKeKzT5in87uf1d5VcT0AnXllhXL4PemvixbPxNE"
+
+# Подключение к Google Sheet
 sheet = client.open_by_key(spreadsheet_id).worksheet("Лист1")
 
+# Выводим первую строку
 print(sheet.row_values(1))
