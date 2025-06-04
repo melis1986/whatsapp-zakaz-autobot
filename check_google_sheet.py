542ac6caa6c657f83b5c47c83e8676593c8fe555
melis1986 <aldaiarovmelis@gmail.com>
2025-06-04 09:44:53 +0400

    Update check_google_sheet.py

--- check_google_sheet.py
+++ check_google_sheet.py
@@ -1,7 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Путь к JSON-файлу
+# Путь к JSON-файлу (если он на сервере Render)
 SERVICE_ACCOUNT_FILE = '/etc/secrets/service_account.json'
 
 # Указываем права доступа
@@ -16,9 +16,10 @@ credentials = Credentials.from_service_account_file(
 )
 
 # Новый ID таблицы
-spreadsheet_id = "1YHAhKeKzT5in87uf1d5VcT0AnXllhXl4PemviXbPxNE"
+spreadsheet_id = '1YHAhKeKzT5in87uf1d5VcT0AnXllhXL4PemviXbPxNE'
 
 # Подключаемся и читаем первую строку
 client = gspread.authorize(credentials)
 sheet = client.open_by_key(spreadsheet_id).worksheet("Лист1")
+
 print(sheet.row_values(1))
