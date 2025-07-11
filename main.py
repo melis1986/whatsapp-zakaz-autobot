d7565ab6694cc0a2bdd92b2aa1268811fe1ae09b
melis1986 <aldaiarovmelis@gmail.com>
2025-07-11 11:16:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, PlainTextResponse, HTMLResponse
 import traceback
 import requests
-gspread
+import gspread
 from google.oauth2.service_account import Credentials
 import os
 from openai import OpenAI
